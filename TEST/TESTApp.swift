35a9b736988f07ed1aa3bba0884b27d154badf0a
QB <usengjake@outlook.com>
2022-05-04 10:57:15 +0800

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by User02 on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
